752b730ef5d073492da18b7ad60241faeb4cd7e6
haple <aleph.oliveira@etec.sp.gov.br>
2021-11-28 01:38:30 -0300

    Fix socket server

--- index.js
+++ index.js
@@ -4,41 +4,29 @@ const cors = require('cors')
 app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
 
-app.use('/peerjs', peerServer);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 io.on('connection', socket => {
-
-    console.log("connection");
+    console.log("CONNECT");
 
     socket.on('join-room', (roomId, userId) => {
 
-        console.log("join-room");
+        console.log("JOIN-ROOM");
         console.log("roomId: " + roomId);
         console.log("userId: " + userId);
-
         socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        // messages
-        socket.on('message', (message) => {
-            //send message to the same room
-            io.to(roomId).emit('createMessage', message)
-        });
+        socket.to(roomId).broadcast.emit('user-connected', userId)
 
         socket.on('disconnect', () => {
-            console.log("join-room");
-            console.log("roomId: " + roomId);
+            console.log("DISCONNECT");
             console.log("userId: " + userId);
 
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
 })
-
 
 server.listen(process.env.PORT || 3333, () => {
     console.log(`🚀 Socket Server started on port ${process.env.PORT || 3333}!`);
