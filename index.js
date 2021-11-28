7155c7df8bff779791ed7134ac1f7175dd900dd3
haple <aleph.oliveira@etec.sp.gov.br>
2021-11-27 21:43:18 -0300

    Update socket server

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-})
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+app.use('/peerjs', peerServer);
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
-        socket.to(roomId).emit('user-connected', userId)
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
 
         socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId)
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
     })
 })
+
 
 server.listen(process.env.PORT || 3333, () => {
     console.log(`🚀 Socket Server started on port ${process.env.PORT || 3333}!`);
